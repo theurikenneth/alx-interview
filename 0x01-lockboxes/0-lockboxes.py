d5b230e6be0d18e5439cba2b7284044dc39bed1f
theurikenneth <theurikenneth@gmail.com>
2022-01-05 11:38:51 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,52 +11,11 @@ def canUnlockAll(boxes):
     boxes start at index 0 and boxes[0] is unlocked
     return true, else false
     """
-    from copy import deepcopy
-
-    # checks if boxes is a positive integer and a list
-    if type(boxes) is not list or len(boxes) < 1:
-        return False
-
-    # checking that all boxes have a valid key
-    for x in boxes:
-        if type(box) is not list:
-            return False
-
-    # deepcopy allows creation of copies without altering originals
-    duplicate_boxes = deepcopy(boxes)
-
-    # creating a list of keys available
-    available_keys = [0]
-    #confirm that there are available using the while loop
-    while len(available_keys) > 0:
-        # setting the first key
-        first_key = available_keys[0]
-        # removing used keys since they are not valid
-        available_keys = available_keys[1:]
-        #confirms that the given first key is valid
-        if type(first_key) is not int or first_key < 0:
-            return False
-
-        # indicate that the box is opened using the -1 flag
-        duplicate_boxes[first_key].append(-1)
-        # check through the opened boxes key and save that
-        for current_key in duplicate_boxes[first_key]:
-            # if its marked -1, it is not valid
-            if current_key is -1:
-                continue
-            # check if it is within range
-            if current_key >= len(duplicate_boxes):
-                continue
-            # continue if the box was already opened
-            if -1 in duplicate_boxes[current_key] or current_key in available keys:
-                continue
-            # append and update the available keys
-            available_keys.append(current_key)
-
-    # confirming all boxes are opened (they have -1 flag)
-    for x in duplicate_boxes:
-        if -1 not in x:
-            return False # shows it was not unlocked
-
-    return True
-
+    box_number = len(boxes)
+    keys_list = boxes[0]
+    locked_box = [False] + [True] * (box_number - 1)
+    for key in keys_list:
+        if ((key < box_number) and (locked_box[key] is True)):
+            locked_box[key] = False
+            keys_list.extend(boxes[key])
+    return not any(locked_box)
