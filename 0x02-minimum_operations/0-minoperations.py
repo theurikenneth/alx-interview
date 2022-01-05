b9d07f7dc589c27a6e7c8ffcd1154b8366794d7c
theurikenneth <theurikenneth@gmail.com>
2022-01-05 11:43:12 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,14 +15,14 @@ def minOperations(n):
     Fewest number of operations
     """
 
-    if n < 2:
+    if n <= 1:
         return 0
     numbr, index, operations = n, 2, 0
 
     while numbr > 1:
         if numbr % index == 0:
             numbr = numbr / index
-            operations = operations + div
+            operations = operations + index
         else:
             index += 1
     return operations
