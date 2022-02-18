b44b32d6e2dc20c1dbaeed4c3acb6c128b0759f9
theurikenneth <theurikenneth@gmail.com>
2022-02-18 21:00:52 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,7 +11,7 @@ def island_perimeter(grid):
     """
     land_perimeter = 0
     for i in range(len(grid)):
-        for j in range(len(grid[a][a])):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if i == 0 or grid[i - 1][j] == 0:
                     land_perimeter += 1
